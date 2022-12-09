f13492c67c29c6f3969158179cdd74ed3312d9c5
PedroLeale <pedroleale@gmail.com>
2022-12-08 23:28:25 -0300

    feat: mint and celo account

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,9 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+const fs = require('fs')
+const path = require('path')
+const privateKeyFile = path.join(__dirname, './.secret')
+const Account = require('./celo_account');
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",

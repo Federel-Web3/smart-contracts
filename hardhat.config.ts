0d2ee87261ce0d48ff63acbb8bd96e38a4ff00ab
pedro bufulin <pedro.almeida@ufu.br>
2022-12-09 21:37:16 -0300

    feat: fix hardhat

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,7 +1,7 @@
+import * as dotenv from "dotenv";
 dotenv.config();
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
-import * as dotenv from "dotenv";
 
 
 const PRIVATE_KEY = process.env.PRIVATE_KEY;

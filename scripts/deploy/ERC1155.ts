e42839632a4a9d8b5b1998a9a7f984f458d970a3
pedro bufulin <pedro.almeida@ufu.br>
2022-12-11 14:44:16 -0300

    feat: deploy and Registry test file

--- scripts/deploy/ERC1155.ts
+++ scripts/deploy/ERC1155.ts
@@ -4,6 +4,11 @@ async function main() {
   const [owner] = await ethers.getSigners();
   console.log("address: ", owner.address);
   console.log("balance: ", (await owner.getBalance()).toString());
+
+  const Immobile = await ethers.getContractFactory("GoodsAndRealEstate");
+  const goodsAndRealEstate = await Immobile.deploy();
+
+  console.log("contract address", goodsAndRealEstate.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere

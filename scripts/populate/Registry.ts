775d793d67a5a5c0d310f6ff6ef1aee50f56af84
pedro bufulin <pedro.almeida@ufu.br>
2022-12-11 20:46:06 -0300

    feat: attach example

--- scripts/populate/Registry.ts
+++ scripts/populate/Registry.ts
@@ -5,8 +5,12 @@ async function main() {
   console.log("address: ", owner.address);
   console.log("balance: ", (await owner.getBalance()).toString());
 
+  const ERC1155 = "0x415d8B75d168d0aa722dAB5CcDB8c122553c4C16";
+  const registryDAO = "0x1723642DC542d480A5D7a323779B6390A78950bF";
+  const registry = "0x06D15664b28d5eCC525ec137E2A30B8Fe25a7567";
+
   const Immobile = await ethers.getContractFactory("GoodsAndRealEstate");
-  const goodsAndRealEstate = await Immobile.deploy();
+  const goodsAndRealEstate = await Immobile.attach(ERC1155);
 
   console.log("contract address", goodsAndRealEstate.address);
 }

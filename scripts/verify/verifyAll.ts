6a2eb034cf602816ebfc7ccca0560ff220a11c1e
pedro bufulin <pedro.almeida@ufu.br>
2022-12-11 21:45:48 -0300

    feat: new addresses

--- scripts/verify/verifyAll.ts
+++ scripts/verify/verifyAll.ts
@@ -3,9 +3,9 @@ import { run, ethers } from "hardhat";
 async function main() {
   const [acc1, acc2] = await ethers.getSigners();
 
-  const ERC1155 = "0x415d8B75d168d0aa722dAB5CcDB8c122553c4C16";
-  const registryDAO = "0x1723642DC542d480A5D7a323779B6390A78950bF";
-  const registry = "0x06D15664b28d5eCC525ec137E2A30B8Fe25a7567";
+  const ERC1155 = "0x70E3b68E7bbdf525babE5B0Ec4d8f3EA676f1345";
+  const registryDAO = "0x3f7C7BA8689bcD40F9bF04a8ce78E95C9BcA43De";
+  const registry = "0xFC22fB792c33d687A41679D62eC2a9E2eEC44A3f";
 
   try {
     await run("verify:verify", {
